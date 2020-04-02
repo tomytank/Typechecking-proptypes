4111b7751593747329610ce49b88b134f94f7a07
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-04-02 23:45:04 +0200

    Updated props values
    Added props values and rendered in React

--- src/components/City.js
+++ src/components/City.js
@@ -1,10 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 const City = props => {
-  console.log(props);
-  return <div>City</div>;
+  return (
+    <div>
+      {props.city.key}
+      {props.city.city}, {props.city.state}
+    </div>
+  );
 };
 City.propTypes = {
+  //shape->describes the object type City has and what data it has
+  city: PropTypes.shape({
+    id: PropTypes.number,
+    city: PropTypes.string,
+    state: PropTypes.string,
+    zip: PropTypes.arrayOf(PropTypes.string)
+  })
   //
 };
 export default City;
